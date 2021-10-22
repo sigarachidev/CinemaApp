930b9e6dfb3406f4cd67ff2dbbbf7c038a25b36d
Maksim Dmitriev <lalalabtv@gmail.com>
2021-10-22 17:31:06 +0300

    update

--- app/src/main/java/com/example/cinemaapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/cinemaapp/fragments/HomeFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cinemaapp.R
-
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 
 class HomeFragment : Fragment() {
+    private lateinit var filmList: MutableList<Objects>
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,4 +24,12 @@ class HomeFragment : Fragment() {
     ): View? {
        return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+
 }
